1e3781c9a0af77d16ed77ba9d3e48ba55f5e8d11
Gordon Chou <chouguting@gmail.com>
2024-04-02 10:39:46 +0800

    add hw2

--- hw2/p1/utils.py
+++ hw2/p1/utils.py
@@ -11,6 +11,7 @@ CAT = ['Kitchen', 'Store', 'Bedroom', 'LivingRoom', 'Office',
 
 CAT2ID = {v: k for k, v in enumerate(CAT)}
 
+
 ########################################
 ###### FEATURE UTILS              ######
 ###### use TINY_IMAGE as features ######
@@ -28,7 +29,7 @@ def get_tiny_images(img_paths):
         1. N is the total number of images
         2. if the images are resized to 16x16, d would be 256
     '''
-    
+
     #################################################################
     # TODO:                                                         #
     # To build a tiny image feature, you can follow below steps:    #
@@ -42,11 +43,20 @@ def get_tiny_images(img_paths):
 
     tiny_img_feats = []
 
+    for img_path in tqdm(img_paths):
+        img = Image.open(img_path)  # 開啟圖片檔
+        img = img.resize((16, 16))  # 調整圖片大小
+        img = np.array(img).astype(np.float32) # 轉換成np.array
+        img = img.flatten()  # 壓縮成一維
+        img = img / np.sum(img)  # 正規化
+        tiny_img_feats.append(img)  # 加入list
+
     #################################################################
     #                        END OF YOUR CODE                       #
     #################################################################
 
     return tiny_img_feats
+
 
 #########################################
 ###### FEATURE UTILS               ######
@@ -54,7 +64,7 @@ def get_tiny_images(img_paths):
 #########################################
 
 ###### Step 1-b-1
-def build_vocabulary(img_paths, vocab_size=400):
+def build_vocabulary(img_paths, vocab_size=150):
     '''
     Input : 
         img_paths (N) : list of string of image paths (training)
@@ -66,7 +76,7 @@ def build_vocabulary(img_paths, vocab_size=400):
         2. vocab_size is up to you, larger value will works better (to a point) 
            but be slower to compute, you can set vocab_size in p1.py
     '''
-    
+
     ##################################################################################
     # TODO:                                                                          #
     # To build vocabularies from training images, you can follow below steps:        #
@@ -97,12 +107,31 @@ def build_vocabulary(img_paths, vocab_size=400):
     # You are welcome to use your own SIFT feature                                   #
     ##################################################################################
 
+    collected_features = []
+    for img_path in tqdm(img_paths):
+        img = Image.open(img_path)  # 開啟圖片檔
+        img = np.asarray(img).astype(np.float32)   # 轉換成np.array
+        descriptors = dsift(img, step=[3, 3], fast=True)[1]  # 取得SIFT特徵
+        # random_indices = np.random.choice(descriptors.shape[0], size=1500,
+        #                                   replace=False)  # 隨機取500個特徵(replace=False代表不重複取樣)
+        # descriptors = descriptors[random_indices]  # 取得500個特徵
+        collected_features.append(descriptors)  # 加入list
+
+    # collected_features目前是一個list，裡面有N張圖片的m個特徵，每個特徵有128維，因此collected_features的shape是(N, m, 128)
+    # 用vstack將list中的array合併，維度變成(N*m, 128)，再轉換成float32
+    np_features = np.vstack(collected_features).astype(np.float32)
+    vocab = kmeans(np_features, num_centers=vocab_size, initialization="PLUSPLUS")  # 進行k-means聚類
+    # 得到的vocab是k個中心點，每個中心點有128維，因此vocab的shape是(k, 128)
+    # k=vocab_size
+    return vocab
+
     ##################################################################################
     #                                END OF YOUR CODE                                #
     ##################################################################################
-    
+
     # return vocab
     return None
+
 
 ###### Step 1-b-2
 def get_bags_of_sifts(img_paths, vocab):
@@ -141,12 +170,31 @@ def get_bags_of_sifts(img_paths, vocab):
     ############################################################################
 
     img_feats = []
+    for img_path in tqdm(img_paths):
+        img = Image.open(img_path)  # 開啟圖片檔
+        img = np.array(img).astype(np.float32)  # 轉換成np.array
+        descriptors = dsift(img, step=[3, 3], fast=True)[1]  # 取得SIFT特徵
+        ## 計算特徵與中心的距離
+        # vocab是k個中心點，每個中心點有128維，因此vocab的shape是(k, 128)
+        # descriptors是m個特徵，每個特徵有128維，因此descriptors的shape是(m, 128)，
+        # (實際上每張圖片的特徵數量不一定相同，但這裡為了方便說明，假設每張圖片的特徵數量都是m)
+        # cdist會計算出 m個128維特徵與k個128維中心的距離，因此distances的shape是(m, k)
+        # 例如，distances[2, 5]代表第2個特徵與第5個中心的距離
+        distances = cdist(descriptors, vocab, metric='cityblock')
+        # 取得每個特徵最近的中心，會得到m個中心的index，因此nearest_centers的shape是(m,)
+        nearest_centers = np.argmin(distances, axis=1)
+        # 計算每個中心的出現次數，hist是一個長度為k的array，代表每個中心的出現次數
+        hist, _ = np.histogram(nearest_centers, bins=len(vocab))
+        #img_feats.append(hist / np.sum(hist))  # 正規化(因為每張圖片的特徵數量不一定相同，因此需要正規化)
+        hist_norm = [float(i) / sum(hist) for i in hist]
+        img_feats.append(hist_norm)
 
     ############################################################################
     #                                END OF YOUR CODE                          #
     ############################################################################
-    
+
     return img_feats
+
 
 ################################################
 ###### CLASSIFIER UTILS                   ######
@@ -200,9 +248,26 @@ def nearest_neighbor_classify(train_img_feats, train_labels, test_img_feats):
     ###########################################################################
 
     test_predicts = []
+    k = 7
+    for test_img_feature in tqdm(test_img_feats):
+
+        # [test_img_feature]是一個 1 x d 的array，train_img_feats是一個 N x d 的array
+        # cdist會計算出 1個d維特徵與N個d維特徵的距離，因此distances的shape是(1, N)，再用squeeze將shape轉換成(N,)
+        # distances[ 3]代表第3個這張testing image與第3個training image的距離
+        distances = cdist(np.asarray([test_img_feature]), train_img_feats, metric='cityblock').squeeze()  # 計算距離
+        nearest_indices = np.argsort(distances)[:k]  # 利用argsort找出距離最小的k個index
+        nearest_labels = [train_labels[i] for i in nearest_indices]  # 取得k個最近的label
+        votingResult = np.zeros(len(CAT))  # 建立一個長度為15的array，用來記錄每個label出現的次數
+        for label in nearest_labels:  # 計算每個label出現的次數
+            votingResult[CAT2ID[label]] += 1
+
+        predict_index = np.argmax(votingResult)  # 取得出現次數最多的label的index
+        predict_label = CAT[predict_index]
+
+        test_predicts.append(predict_label)  # 加入list
 
     ###########################################################################
     #                               END OF YOUR CODE                          #
     ###########################################################################
-    
+
     return test_predicts
